79a0d2db86b524706563aeee478a0873a3c4fffa
Kevin <hellokevinvogel@gmail.com>
2022-03-16 08:31:45 +0100

    config(typeorm): add connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'micro_order',
+      username: 'kevin',
+      password: null,
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
